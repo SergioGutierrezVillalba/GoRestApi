81252f6e791bb9d06aafc18bf1869c36cd43db10
SergioG <gv.sergio@gmail.com>
2019-04-11 09:50:06 +0200

    fix(#19):Disallow permissions of changing password of other user when Role is Admin

--- Domains/user/usecase/usecase.go
+++ Domains/user/usecase/usecase.go
@@ -17,6 +17,8 @@ type Usecase interface{
 	GetUserByRecoverToken(string)(model.User, error)
 	Create(model.User) error
 	Update(model.User) error
+	UpdateSelf(model.User) error
+	UpdateAdmin(model.User) error
 	Delete(string) error
 	Register(model.User) error
 }
@@ -134,6 +136,40 @@ func (u *UsersUsecase) Update(user model.User) (err error) {
 		return
 	}
 	return
+}
+
+func (u *UsersUsecase) UpdateSelf(user model.User) (err error){
+
+	var fieldsRequired []string
+	fieldsRequired = append(fieldsRequired, user.Username, user.Password, user.Email, user.GroupId)
+
+	if !checker.HasFieldsRequired(fieldsRequired){
+		err = errors.New("EmptyFieldsError")
+		return
+	}
+
+	if err = u.repo.Update(user); err != nil {
+		err = errors.New("UpdateUserError")
+		return
+	}
+	return
+}
+
+func (u *UsersUsecase) UpdateAdmin(user model.User) (err error){
+
+	var fieldsRequired  []string
+	fieldsRequired = append(fieldsRequired, user.Username, user.Role, user.Email, user.GroupId)
+
+	if !checker.HasFieldsRequired(fieldsRequired){
+		err = errors.New("EmptyFieldsError")
+		return
+	}
+
+	if err = u.repo.Update(user); err != nil {
+		err = errors.New("UpdateUserError")
+		return
+	}
+	return 
 }
 
 func (u *UsersUsecase) Delete(userId string) (err error) {
